aa3b61ded91d482b153c59e430e2d088f5cd139c
Mansur Akidah <akidahmansur786@gmail.com>
2024-10-16 12:37:33 +0300

    Another dictionary solution for length of substring

--- LengthOfLongestSubstring.cs
+++ LengthOfLongestSubstring.cs
@@ -11,21 +11,33 @@ namespace Leets
         public int Substring(string s)
         {
             Dictionary<char, int> dict = new();
-
             int start = 0;
             int max = 0;
 
-            for(int i = 0; i < s.Length; i++)
-            {
-                if (dict.ContainsKey(s[i]) && dict[s[i]] >= start)
-                {
-                    start = dict[s[i]] + 1;
-                }
-                dict[s[i]] = i;
+            Console.WriteLine($"Input string: {s}");
+            Console.WriteLine("Index\tChar\tWindow\t\tStart\tMax\tDictionary");
+            Console.WriteLine("-----\t----\t------\t\t-----\t---\t----------");
 
+            for (int i = 0; i < s.Length; i++)
+            {
+                char currentChar = s[i];
+
+
+                if (dict.ContainsKey(currentChar) && dict[currentChar] >= start)
+                {
+                    start = dict[currentChar] + 1;
+                    Console.WriteLine($"Updating start to {start} ");
+                }
+
+                dict[currentChar] = i;
                 max = Math.Max(max, i - start + 1);
-                
+
+                string window = s.Substring(start, i - start + 1);
+                Console.Write($"{i}\t{currentChar}\t{window,-10}\t{start}\t{max}\t");
+                // Print dictionary content
+                Console.WriteLine(string.Join(", ", dict.Select(kvp => $"{kvp.Key}:{kvp.Value}")));
             }
+
             return max;
 
         }
